d5d39a782a0bc70301fefd0a0e9cec1b34fbfb0f
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-09 15:51:31 -0600

    added background image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,13 @@
 	import { ProgressSection, TodoSection, AddTodoSection } from '$lib/components';
 </script>
 
-<div class="min-h-screen bg-mint-green p-0 selection:bg-picton-blue md:py-24 md:px-8">
+<div
+	class="selection:bg-picton-blue min-h-screen bg-[url('$lib/img/img-bg.png')] bg-cover p-0 md:py-24 md:px-8"
+>
 	<main
-		class="mx-auto min-h-screen max-w-none rounded-none bg-alice-blue px-5 py-10 outline-none md:min-h-max md:max-w-[60rem] md:rounded-2xl md:px-8 md:outline md:outline-4 md:outline-offset-8 md:outline-alice-blue"
+		class="bg-alice-blue md:outline-alice-blue mx-auto min-h-screen max-w-none rounded-none px-5 py-10 outline-none md:min-h-max md:max-w-[60rem] md:rounded-2xl md:px-8 md:outline md:outline-4 md:outline-offset-8"
 	>
-		<h1 class="text-2xl font-bold text-indigo-dye">Todo App</h1>
+		<h1 class="text-indigo-dye text-2xl font-bold">Todo App</h1>
 		<div class="mt-10">
 			<AddTodoSection />
 			<TodoSection />
