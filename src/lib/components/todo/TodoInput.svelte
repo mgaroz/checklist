bd90f9da8e889456dbf56cca9ce5a908458c5641
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-09 03:09:30 -0600

    fixed text color

--- src/lib/components/todo/TodoInput.svelte
+++ src/lib/components/todo/TodoInput.svelte
@@ -2,10 +2,12 @@
 	import { deleteTodo, toggleComplete, editTodo } from '$lib/store/TodoStore';
 
 	export let todo;
-	$: completeClass = todo.complete ? 'bg-moonstone text-alice-blue' : 'bg-mint-green';
+	$: completeClass = todo.complete
+		? 'bg-moonstone text-alice-blue'
+		: 'bg-mint-green text-indigo-dye';
 </script>
 
-<div class="flex items-center justify-between rounded-md border-2 border-moonstone px-5 py-4">
+<div class="border-moonstone flex items-center justify-between rounded-md border-2 px-5 py-4">
 	<div class="flex w-full max-w-lg items-center justify-start">
 		<label for={`${todo.id}-checkbox`} class="sr-only">Complete todo</label>
 		<input
@@ -13,7 +15,7 @@
 			checked={todo.complete}
 			id={`${todo.id}-checkbox`}
 			on:change={() => toggleComplete(todo.id)}
-			class="h-4 w-4 rounded border-moonstone bg-alice-blue text-moonstone focus:border-moonstone focus:outline focus:outline-1 focus:outline-offset-2 focus:outline-moonstone transition-all duration-100"
+			class="border-moonstone bg-alice-blue text-moonstone focus:border-moonstone focus:outline-moonstone h-4 w-4 rounded transition-all duration-100 focus:outline focus:outline-1 focus:outline-offset-2"
 		/>
 		<label for={`${todo.id}-text`} class="sr-only">Edit todo</label>
 		<input
@@ -22,20 +24,18 @@
 			placeholder="Enter a todo"
 			value={todo.text}
 			on:input={(e) => editTodo(todo.id, e.target.value)}
-			class="ml-5 flex-1 text-ellipsis rounded-none border-x-0 border-t-0 border-b border-dashed border-picton-blue bg-alice-blue px-0 pb-1 text-base font-normal text-indigo-dye placeholder:text-moonstone focus:border-indigo-dye focus:outline-none focus:ring-0 font-[Inter]"
+			class="border-picton-blue bg-alice-blue text-indigo-dye placeholder:text-moonstone focus:border-indigo-dye ml-5 flex-1 text-ellipsis rounded-none border-x-0 border-t-0 border-b border-dashed px-0 pb-1 font-[Inter] text-base font-normal focus:outline-none focus:ring-0"
 		/>
-		<span
-			class="{completeClass} ml-5 hidden rounded-full py-0.5 px-2 text-sm font-normal md:block text-indigo-dye"
-		>
+		<span class="{completeClass} ml-5 hidden rounded-full py-0.5 px-2 text-sm font-normal md:block">
 			{todo.complete ? 'Complete' : 'In progress'}
 		</span>
 	</div>
 	<button
 		type="button"
 		on:click={() => deleteTodo(todo.id)}
-		class="group ml-4 flex items-center justify-center rounded-md bg-alice-blue p-2 hover:bg-picton-blue hover:text-alice-blue focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-dye text-indigo-dye"
+		class="bg-alice-blue hover:bg-picton-blue hover:text-alice-blue focus-visible:outline-indigo-dye text-indigo-dye group ml-4 flex items-center justify-center rounded-md p-2 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2"
 	>
 		<span class="sr-only">Delete todo</span>
-		<i class="fa-solid fa-trash text-lg h-7 w-8" />
+		<i class="fa-solid fa-trash h-7 w-8 text-lg" />
 	</button>
 </div>
