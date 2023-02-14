07eb89e3ee6a552a4a34c32cf043cc7cbd380712
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-14 16:15:42 -0600

    replaced bg with webp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="selection:bg-picton-blue min-h-screen bg-[url('$lib/img/img-bg.png')] bg-cover p-0 md:py-24 md:px-8"
+	class="selection:bg-picton-blue min-h-screen bg-[url('$lib/img/img-bg.webp')] bg-cover p-0 md:py-24 md:px-8"
 >
 	<main
 		class="bg-alice-blue md:outline-alice-blue mx-auto min-h-screen max-w-none rounded-none px-5 py-10 outline-none md:min-h-max md:max-w-[60rem] md:rounded-2xl md:px-8 md:outline md:outline-4 md:outline-offset-8"
