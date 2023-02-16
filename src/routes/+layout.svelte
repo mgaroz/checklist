7c57da2af64b1888a85dce1d4c7bf1768199454c
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-16 01:44:38 -0600

    fa load

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,13 @@
 		crossorigin="anonymous"
 		referrerpolicy="no-referrer"
 	/>
+	<link
+		rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/solid.min.css"
+		integrity="sha512-bdTSJB23zykBjGDvyuZUrLhHD0Rfre0jxTd0/jpTbV7sZL8DCth/88aHX0bq2RV8HK3zx5Qj6r2rRU/Otsjk+g=="
+		crossorigin="anonymous"
+		referrerpolicy="no-referrer"
+	/>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 	<link
